29574128c614471e0b347315d996a1d948b19222
凯文 胡 <lostkevin666@gmail.com>
2019-09-04 22:05:03 +0800

    test

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ from nonebot.default_config import *
 SUPERUSERS={1870697069}
 COMMAND_START = {'', '/','／'}
 DEBUG = True
+PROGRAMMERS = {1870697069}
+REPO = 'https://github.com/lostkevin/EisuijoshiBot.git' #bot repo
