db665315f370216c7054711dcf3ee006e7b5032f
lostkevin <46404040+lostkevin@users.noreply.github.com>
2019-09-04 22:46:33 +0800

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -2,6 +2,6 @@ from nonebot import on_command, CommandSession
 
 
 # on_command 装饰器将函数声明为一个命令处理器
-@on_command('ECHO', only_to_me=False)
+@on_command('testECHO', only_to_me=False)
 async def echo(session: CommandSession):
     await session.send(session.state.get('message') or session.current_arg)
