042483a42dcb00b13db6aa9a5c2fadbc3cafdb92
lostkevin <46404040+lostkevin@users.noreply.github.com>
2019-09-04 23:22:41 +0800

    Update newcmd.py

--- plugins/newcmd.py
+++ plugins/newcmd.py
@@ -3,6 +3,6 @@ from nonebot import on_command, CommandSession
 
 # on_command 装饰器将函数声明为一个命令处理器
 
-@on_command('test', only_to_me=False)
+@on_command('aaa', only_to_me=False)
 async def echo(session: CommandSession):
     await session.send(session.state.get('message') or session.current_arg)
