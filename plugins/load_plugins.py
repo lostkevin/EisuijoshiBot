591c35faaf0852c0029a4b41f6dd3126f759b41b
凯文 胡 <lostkevin666@gmail.com>
2019-09-05 15:14:52 +0800

    fix reboot plugin

--- plugins/load_plugins.py
+++ plugins/load_plugins.py
@@ -6,7 +6,7 @@ async def reboot(session: CommandSession):
     if hasattr(session.bot.config, 'PROGRAMMERS'):
         if session.ctx['sender']['user_id'] in session.bot.config.PROGRAMMERS:
             try:
-                subprocess.check_call('git pull %s' % session.bot.config.REPO)
+                subprocess.check_call(['git', 'pull' , session.bot.config.REPO])
                 load_plugins('./plugins','plugins', True)
                 await session.send('更新成功')
             except subprocess.CalledProcessError:
