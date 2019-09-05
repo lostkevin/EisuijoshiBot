5717b0893e41a94e6a4769cbfe390084e7475c2a
凯文 胡 <lostkevin666@gmail.com>
2019-09-05 22:28:08 +0800

    modify config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from nonebot.default_config import *
 
-SUPERUSERS={1870697069}
+SUPERUSERS={1870697069, 3133524514, 826818663, 215941768}
 COMMAND_START = {'', '/','／'}
 DEBUG = True
-PROGRAMMERS = {1870697069}
+PROGRAMMERS = {1870697069, 3133524514, 826818663, 215941768}
 REPO = 'https://github.com/lostkevin/EisuijoshiBot.git' #bot repo
 ALLOWED_GROUP = []
 print("Config Loaded")
