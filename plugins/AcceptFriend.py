567c0dd0360c1077bb834961ac257899528df370
凯文 胡 <lostkevin666@gmail.com>
2020-03-02 20:41:49 +0800

    增加PCR接龙小游戏功能

--- plugins/AcceptFriend.py
+++ plugins/AcceptFriend.py
@@ -1,7 +1,12 @@
 from nonebot import on_request, RequestSession, get_bot, on_command, CommandSession
 
+_state = False
+
 @on_request('friend')
 async def _(session: RequestSession):
+    if hasattr(session.bot.config, 'friendState') and session.bot.config.friendState:
+        await session.approve()
+        return
     if str(session.ctx['user_id']) in session.bot.config.ALLOWED_FRIEND:
         await session.approve()
     else:
@@ -10,5 +15,9 @@ async def _(session: RequestSession):
 @on_command('friend', only_to_me=True)
 async def _(session: CommandSession):
     if session.ctx['sender']['user_id'] in session.bot.config.SUPERUSERS:
+        if session.current_arg == 'off':
+            session.bot.config.state = True
+        if session.current_arg == 'on':
+            session.bot.config.state = False
         session.bot.config.ALLOWED_FRIEND.append(session.current_arg)
         await session.send("添加成功!")
---
+++ plugins/jielong.py
@@ -0,0 +1,90 @@
+from pypinyin import lazy_pinyin, Style
+from utils.listm import yuntop,ptoyun,listma,idicon
+from nonebot import CommandSession, on_command
+from os import path, mkdir
+
+yuntoicron = {}
+same = ['jh','ch','sh','r','z','c','s']
+xiuzhen = {
+'iu':'ou' ,'uen':'un',
+'ve':'yue','van':'yuan','vn':'yun'
+}
+same2 = {'zhi':'jh','chi':'ch','shi':'sh','ri':'r','zi':'z','ci':'c','si':'s'}
+
+from PIL import Image
+im = Image.open("./resource/icons.png")
+for i in listma:
+    y = i[3]
+    p = yuntop[y]
+
+    if p in same:
+        # print(p)
+        p = 'same'
+
+    if p not in yuntoicron:
+        # print(p)
+        yuntoicron[p] = [i[0]]
+    else:
+        yuntoicron[p].append(i[0])
+
+
+def trans_text_tolist(text):
+    tailtext = text[-1]
+    a = lazy_pinyin(tailtext, Style.FINALS)
+    b = lazy_pinyin(tailtext)
+    # print(a[-1],b[-1])
+    p = a[-1]
+    if b[-1] in same2:
+        p = 'same'
+    elif a[-1] in xiuzhen:
+        p = xiuzhen[a[-1]]
+    return yuntoicron[p]
+
+def get_single_icon(id_):
+    index = idicon[id_]
+    box = (index[0]+2,index[1]+2,index[0]+78,index[1]+78)
+    region = im.crop(box)
+    return region
+
+def image_merge(images): 
+    width, height = 76,76
+    nums = len(images)
+    if nums > 5:
+        new_width = 5 * width
+        new_height = int(1 + nums/5) * height
+
+    new_img = Image.new('RGB', (new_width, new_height), (255,255,255)) 
+
+    x = y = 0
+    for img in images: 
+        new_img.paste(img, (x, y)) 
+        x += width 
+        if x >= width * 5:
+            x = 0
+            y += height
+    return new_img
+  
+
+@on_command('接龙', only_to_me=True)
+async def _(commandSession: CommandSession):
+    x = trans_text_tolist(commandSession.current_arg)
+    imgs = []
+    for i in x:
+        imgs.append(get_single_icon(i))
+    result = image_merge(imgs)
+    if not path.isdir('./data/tmp'):
+        mkdir('./data/tmp')
+    result.save(path.join("./data/tmp", "jielong.png"))
+    p = path.abspath(path.join("./data/tmp", "jielong.png"))
+    await commandSession.send("[CQ:image,file=file:///" + ("Z:/" if p.startswith('/') else "") + p + "]")
+    return
+
+
+
+
+
+
+
+
+
+
