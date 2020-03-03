44ad791e8731366e7b6b2963e3bc4def956a52ba
凯文 胡 <lostkevin666@gmail.com>
2020-03-03 19:04:45 +0800

    接龙功能现在能根据目标自动排序最优选项了

--- plugins/jielong.py
+++ plugins/jielong.py
@@ -2,9 +2,10 @@ from pypinyin import lazy_pinyin, Style
 from utils.listm import yuntop,ptoyun,listma,idicon
 from nonebot import CommandSession, on_command
 from os import path, mkdir
-
+from utils.JielongSearch import choiceEval
 yuntoicron = {}
 same = ['jh','ch','sh','r','z','c','s']
+same3 = ['ㄓ', 'ㄔ', 'ㄕ', 'ㄖ', 'ㄗ', 'ㄘ', 'ㄙ']
 xiuzhen = {
 'iu':'ou' ,'uen':'un',
 've':'yue','van':'yuan','vn':'yun'
@@ -18,15 +19,21 @@ for i in listma:
     p = yuntop[y]
 
     if p in same:
-        # print(p)
         p = 'same'
 
     if p not in yuntoicron:
-        # print(p)
-        yuntoicron[p] = [i[0]]
+        yuntoicron[p] = [[i[0], i[4]]]
     else:
-        yuntoicron[p].append(i[0])
+        yuntoicron[p].append([i[0], i[4]])
 
+def normalizePinyin(text):
+    tailtext = text[-1]
+    a = lazy_pinyin(tailtext, Style.FINALS)
+    b = lazy_pinyin(tailtext)
+    p = same2[b[-1]] if b[-1] in same2 else a[-1]
+    if a[-1] in xiuzhen:
+        p = xiuzhen[a[-1]]
+    return p
 
 def trans_text_tolist(text):
     tailtext = text[-1]
@@ -46,7 +53,7 @@ def get_single_icon(id_):
     region = im.crop(box)
     return region
 
-def image_merge(images): 
+def image_merge(images, score):
     width, height = 76,76
     nums = len(images)
     if nums > 5:
@@ -54,14 +61,23 @@ def image_merge(images):
         new_height = int(1 + nums/5) * height
 
     new_img = Image.new('RGB', (new_width, new_height), (255,255,255)) 
-
+    res = sorted(score.items(), key=lambda x:x[1])
     x = y = 0
-    for img in images: 
-        new_img.paste(img, (x, y)) 
-        x += width 
-        if x >= width * 5:
-            x = 0
-            y += height
+    if len(res) > 0:
+        for tuple in res:
+            img = eval(tuple[0])
+            new_img.paste(img, (x, y))
+            x += width
+            if x >= width * 5:
+                x = 0
+                y += height
+    else:
+        for img in images:
+            new_img.paste(img, (x, y))
+            x += width
+            if x >= width * 5:
+                x = 0
+                y += height
     return new_img
   
 @on_command('接龙公屏')
@@ -73,11 +89,24 @@ async def _(session: CommandSession):
 async def _(session: CommandSession):
     if hasattr(session.bot.config, 'jielongState') and session.bot.config.jielongState:
         return
-    x = trans_text_tolist(session.current_arg)
+    session.current_arg = session.current_arg.split(' ')
+    x = trans_text_tolist(session.current_arg[0])
+    res = {}
+    if len(session.current_arg) > 1:
+        pin = [session.current_arg[0][-1], session.current_arg[1][0]]
+        yun = [
+            ptoyun[normalizePinyin(pin[i])] for i in range(0, len(pin))
+        ]
+        res = choiceEval(yun[0], yun[1])
     imgs = []
+    levels = {}
+    count = 0
     for i in x:
-        imgs.append(get_single_icon(i))
-    result = image_merge(imgs)
+        imgs.append(get_single_icon(i[0]))
+        if len(session.current_arg) > 1:
+            levels['images[%d]' % count] = res['same' if i[1] in same3 else i[1]]
+            count+=1
+    result = image_merge(imgs, levels)
     if not path.isdir('./data/tmp'):
         mkdir('./data/tmp')
     result.save(path.join("./data/tmp", "jielong.png"))
